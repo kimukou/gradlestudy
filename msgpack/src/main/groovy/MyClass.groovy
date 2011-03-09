1bbea3da79244ceb0f3baab7f3a0d0f38742bf92
kimukou_26 <kimukou.buzz@gmail.com>
2011-03-09 13:29:46 +0900

    update test

---
+++ msgpack/src/main/groovy/MyClass.groovy
@@ -0,0 +1,16 @@
+import org.msgpack.MessagePack
+import org.msgpack.annotation.MessagePackMessage
+import org.msgpack.annotation.Optional
+// ユーザー定義のクラス
+// アノテーションを付ける
+@MessagePackMessage
+public class MyClass {
+
+		//[NOTE]変数にpublic宣言がないと上手くシリアライズ、デシリアライズが出来ないみたい
+    public String str
+    public double num
+
+		// new field
+    @Optional
+    public int flag = 0
+}
