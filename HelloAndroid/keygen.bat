261aa4268a5a7ba70ab713b9dbf77e055e6696c3
kimukou_26 <kimukou.buzz@gmail.com>
2011-03-12 01:11:45 +0900

    Signed-off-by: kimukou_26 <kimukou.buzz@gmail.com>

--- HelloAndroid/keygen.bat
+++ HelloAndroid/keygen.bat
@@ -1,5 +1,6 @@
 set JAVA_HOME=c:\opt\jdk
 set PATH=%JAVA_HOME%/bin
 
-echo "android type"
-keytool -list -keystore "%USERPROFILE%/.android/debug.keystore"
+echo "android type keyStorePassword and  keyAliasPassword"
+keytool -genkey -alias androiddebugkey -keystore debug.keystore
+
