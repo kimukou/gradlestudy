322a78b9f01268147a0c935da41e943576bd3e73
kimukou_ <kimukou.buzz@gmail.com>
2011-04-11 12:02:13 +0900

    UTF-8 apply

--- HelloAndroid/apk_uninstall.bat
+++ HelloAndroid/apk_uninstall.bat
@@ -2,6 +2,7 @@ set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
 set JAVA_HOME=c:\opt\jdk
 set ANDROID_HOME=D:/Tooldev/android-sdk-windows
 
+set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin;%ANDROID_HOME%/platform-tools;%ANDROID_HOME%/tools
 
 echo "need prepare 'android' command and emurator running"
--- testter/adb_lunch.bat
+++ testter/adb_lunch.bat
@@ -2,6 +2,7 @@ set ANT_HOME=D:/Tooldev/ant
 set JAVA_HOME=c:\opt\jdk
 set ANDROID_HOME=D:/Tooldev/android-sdk-windows
 set PATH=%ANT_HOME%/bin;%JAVA_HOME%/bin;%ANDROID_HOME%/platform-tools;%ANDROID_HOME%/tools
+set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
 ::see http://www.neko.ne.jp/~freewing/android/android/
 ::    http://dev.ariel-networks.com/Members/iwanaga/android-intro/
