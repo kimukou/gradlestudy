731c9a6006c70fae07286a99a60d6c09e9fc8483
kimukou_ <kimukou.buzz@gmail.com>
2011-04-19 18:01:20 +0900

    freemind plugin compile test

---
+++ freemind_script_plugin/package.bat
@@ -0,0 +1,12 @@
+::パッケージングバッチファイル
+
+set GROOVY_HOME=C:\opt\groovy-1.7.10
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
+set JAVA_HOME=c:\opt\jdk
+set PATH=%GROOVY_HOME%/bin;%GRADLE_HOME%/bin;%JAVA_HOME%/bin
+
+set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
+
+
+::call gradle test
+call gradle clean jar
