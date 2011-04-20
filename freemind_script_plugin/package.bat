8db5e0b83ca5feec9fdaaba22b8f08f59fe77c27
kimukou_ <kimukou.buzz@gmail.com>
2011-04-20 17:45:42 +0900

    testcode copy

--- freemind_script_plugin/package.bat
+++ freemind_script_plugin/package.bat
@@ -3,7 +3,9 @@
 set GROOVY_HOME=C:\opt\groovy-1.7.10
 set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
 set JAVA_HOME=c:\opt\jdk
-set PATH=%GROOVY_HOME%/bin;%GRADLE_HOME%/bin;%JAVA_HOME%/bin
+set ANT_HOME=D:\Tooldev\ant
+
+set PATH=%GROOVY_HOME%/bin;%GRADLE_HOME%/bin;%JAVA_HOME%/bin;%ANT_HOME%/bin
 
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
