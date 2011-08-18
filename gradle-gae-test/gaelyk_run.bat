03780cb3645163d74f9715306b7882f8303cc929
kimukou_26 <kimukou.buzz@gmail.com>
2011-08-18 23:14:46 +0900

    upgrade gae plugin

--- gradle-gae-test/gaelyk_run.bat
+++ gradle-gae-test/gaelyk_run.bat
@@ -1,8 +1,4 @@
-set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
-set JAVA_HOME=c:\opt\jdk
-set APPENGINE_HOME=D:\Tooldev\appengine-java-sdk-1.5.0
-
-set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin;%APPENGINE_HOME%/bin
+call ../setEnvGG.bat
 
 
 call gradle -b gaelyk-build.gradle war %*
