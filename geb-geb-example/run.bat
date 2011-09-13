df6c5741169524df069ba9e12899de3ade9acdab
kimukou <kimukou.buzz@gmail.com>
2011-09-13 19:01:35 +0900

    taskkilling setting

--- geb-geb-example/run.bat
+++ geb-geb-example/run.bat
@@ -1,6 +1,13 @@
 ::call ../setEnvGG.bat
 ::gradle chromeTest
 
+::task killing
+::taskkill /F /IM chromedriver.exe
+::taskkill /F /IM chrome.exe
+.\exe\taskkill0 /F /IM chromedriver.exe
+.\exe\taskkill0 /F /IM chrome.exe
+
+
 set JAVA_HOME=c:\opt\jdk
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 gradlew clean chromeTest
