d8cdb9270ea6dcdd912f2ed11f1c807501242d21
kimukou_26 <kimukou.buzz@gmail.com>
2011-10-04 07:25:45 +0900

    gradle.startParameter.taskNames switch setting

--- geb-geb-example/adb_create.bat
+++ geb-geb-example/adb_create.bat
@@ -3,4 +3,4 @@ call ../setEnvGG.bat
 ::see 
 ::       http://dev.ariel-networks.com/Members/iwanaga/android-intro/
 
-android create avd -n my_android -t 12 -c 100M
+android create avd -n my_android -t 12
