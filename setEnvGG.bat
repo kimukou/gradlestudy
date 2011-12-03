1bbdb719cae247880fb89d2dac40d4872279266b
kimukou <kimukou.buzz@gmail.com>
2011-12-03 18:01:20 +0900

    up

--- setEnvGG.bat
+++ setEnvGG.bat
@@ -1,12 +1,12 @@
 set JAVA_HOME=c:\opt\jdk
 
-set GRADLE_HOME=D:\ToolDev\gradle-1.0-milestone-4
+set GRADLE_HOME=D:\ToolDev\gradle-1.0-milestone-6
 set GIT_HOME=D:\ToolDev\Git
 set MVN_HOME=D:\Tooldev\apache-maven-3.0.3
 set ANT_HOME=D:\Tooldev\ant
 
 set ANDROID_HOME=D:/Tooldev/android-sdk-windows
-set APPENGINE_HOME=D:\Tooldev\appengine-java-sdk-1.5.4
+set APPENGINE_HOME=D:\Tooldev\appengine-java-sdk-1.5.5
 
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
