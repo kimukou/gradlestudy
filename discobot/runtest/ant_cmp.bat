1756dfbec37c680b74245b7f0d802a1c21bc01df
kimukou <kimukou.buzz@gmail.com>
2011-12-27 07:37:48 +0900

    discobot updating jar(use github project

--- discobot/runtest/ant_cmp.bat
+++ discobot/runtest/ant_cmp.bat
@@ -3,5 +3,6 @@ set JAVA_HOME=c:\opt\jdk
 set ANDROID_HOME=D:/Tooldev/android-sdk-windows
 set PATH=%ANT_HOME%/bin;%JAVA_HOME%/bin;%ANDROID_HOME%/platform-tools;%ANDROID_HOME%/tools
 
-set CLASSPATH=libs/discobot.jar
+::set CLASSPATH=libs/discobot.jar
+set CLASSPATH=libs/groovy-1.9.0-beta-1-SNAPSHOT.jar
 ant
--- setEnvGG.bat
+++ setEnvGG.bat
@@ -5,10 +5,11 @@ set GIT_HOME=D:\ToolDev\Git
 set MVN_HOME=D:\Tooldev\apache-maven-3.0.3
 set ANT_HOME=D:\Tooldev\ant
 
-set ANDROID_HOME=D:/Tooldev/android-sdk-windows
+set ANDROID_HOME=D:\Tooldev\android-sdk-windows
 set APPENGINE_HOME=D:\Tooldev\appengine-java-sdk-1.6.0
 
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
+set JAVA_OPTS=%JAVA_OPTS% -DproxyHost=192.168.1.102 -DproxyPort=9090
 
 set PATH=%JAVA_HOME%/bin
 set PATH=%PATH%;%GRADLE_HOME%\bin
