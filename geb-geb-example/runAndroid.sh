b8fde42fc7fb9ab860ed3f2de716c7a4cbf70a44
kimukou_26 <kimukou.buzz@gmail.com>
2012-11-08 17:52:54 +0900

    update gredle wrapper

---
+++ geb-geb-example/runAndroid.sh
@@ -0,0 +1,39 @@
+#call ../setEnvGG.bat
+
+
+#::start emulator -avd my_android  -no-window -no-audio -no-boot-anim
+start emulator -avd my_android  -no-audio -no-boot-anim
+
+echo "waiting emurator rised..."
+pause 
+
+adb -e install -r  apk\android-server-2.13.0.apk
+
+#::
+#:: apk start
+#::
+#:: see http://code.google.com/p/selenium/source/browse/trunk/android/app/AndroidManifest.xml
+#::     http://d.hatena.ne.jp/Kazzz/20100909/p1
+#::
+#:: check for http://localhost:8080/wd/hub/status/ shown
+adb shell am start -n org.openqa.selenium.android.app/.MainActivity
+
+adb forward tcp:8080 tcp:8080
+
+#::pause
+
+./gradlew clean androidTest -DandroidTest.single=OneTest
+
+
+#:: see http://code.google.com/p/selenium/wiki/AndroidDriver
+#::
+#:: Install Android WebDriver on Real Device
+#:: Connect your device through USB to your machine.
+#:: 
+#:: $~/android_sdk/platform-tools/adb devices
+#:: This will give you the serial Id of your connected device. Now install the apk on the device:
+#:: 
+#:: $./adb -s <serialId> -e install -r android-server.apk
+#:: Setup port forwarding:
+#:: 
+#:: $~/android_sdk/adb -s <serialId> forward tcp:8080 tcp:8080
